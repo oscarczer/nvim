1b3e94af8393596c1cdaaf6343cc2f4b9ffb594d
Oscar Czernuszyn <oscarczer@gmail.com>
2025-02-05 10:53:27 +1100

    Update init.lua

--- init.lua
+++ init.lua
@@ -191,8 +191,8 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Recentre cursor on large scrolls
-vim.keymap.set('n', '<C-d>', '<C-d>zz>')
-vim.keymap.set('n', '<C-u>', '<C-u>zz>')
+vim.keymap.set('n', '<C-d>', '<C-d>zz>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz>', { noremap = true, silent = true })
 
 -- Recentre cursor on normal scroll
 vim.api.nvim_set_keymap('n', 'j', 'jzz', { noremap = true, silent = true })
